33e1be36f9279fd7601d4429e1d5b11cafaade7b
Rehan Suhail <rehansuhail19@gmail.com>
2025-09-02 18:57:49 +0530

    V8

--- server/index.js
+++ server/index.js
@@ -3,10 +3,9 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import serverless from "serverless-http";
 
 import postRoutes from "./routes/posts.js";
-import userRoutes from "./routes/users.js";
+import userRoutes from "./routes/users.js"; // placeholder route
 
 dotenv.config();
 
@@ -23,7 +22,7 @@ app.use("/api/user", userRoutes);
 
 // Test route
 app.get("/", (req, res) => {
-  res.send("Backend is running on Vercel!");
+  res.send("✅ Backend is running on Vercel!");
 });
 
 // MongoDB connection
@@ -32,8 +31,8 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("MongoDB connected"))
+  .then(() => console.log("✅ MongoDB connected"))
   .catch((error) => console.log(`${error} did not connect`));
 
-// Export for Vercel (Serverless)
-export const handler = serverless(app);
+// 👉 For Vercel: export app as default
+export default app;
