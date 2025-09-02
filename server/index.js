34c5d618b1a7abe1a241acc6c8383cd2f3d009a8
Rehan Suhail <rehansuhail19@gmail.com>
2025-09-02 17:36:19 +0530

    Up 2

--- server/index.js
+++ server/index.js
@@ -1,28 +1,36 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
 
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
 
-import postRoutes from './routes/posts.js';
-
-const app = express();
 dotenv.config();
 
+const app = express();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use('/posts', postRoutes);
+app.use("/posts", postRoutes);
+app.use("/user", userRoutes);
 
-app.get('/', (req,res) => {
-    res.send('Hello to Echo API');
-});
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT|| 5000;
-
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`✅ Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
   .catch((error) => console.log(`${error} did not connect`));
+
+// ✅ Export app for Vercel
+export default app;
